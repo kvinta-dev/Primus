f46d389ef50c6fb7fcda2fffc42552ae25fbc92d
Gennady Nimchenko <nimchenko@gmail.com>
2020-02-01 22:44:08 +0300

    Косметические правки

--- PycharmProjects/parsing/1/main.py
+++ PycharmProjects/parsing/1/main.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 
 
 def get_html(url):
-	r = requests.get(url)
-	return r.text
+    r = requests.get(url)
+    return r.text
 
 
 def get_data(html):
-	soup = BeautifulSoup(html, 'lxml')
-	h1 = soup.find('h1', class_='entry-title').text
-	return h1
+    soup = BeautifulSoup(html, 'lxml')
+    h1 = soup.find('h1', class_='entry-title').text
+    return h1
 
 
 def main():
@@ -18,9 +18,6 @@ def main():
     print(get_data(get_html(url)))
 
 
-
-
-
 if __name__ == '__main__':
-	main()
+    main()
 
--- PycharmProjects/parsing/2/main.py
+++ PycharmProjects/parsing/2/main.py
@@ -4,49 +4,45 @@ import csv
 
 
 def get_html(url):
-	r = requests.get(url)
-	return r.text
+    r = requests.get(url)
+    return r.text
 
 
 def refined(s):
-	r = s.split(' ')[0]
-	print(r)
-	return r
-	# return r.replase(',', '')
+    r = s.split(' ')[0]
+    print(r)
+    return r
+    # return r.replase(',', '')
+
 
 def write_csv(data):
     with open('plugins.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(data)
-		# writer.writerow(data['name'], data['url'], data['reviews'])
+        # writer.writerow(data['name'], data['url'], data['reviews'])
 
 
 
 def get_data(html):
-	soup = BeautifulSoup(html, 'lxml')
-	popular = soup.find_all('section')[1]
-	plugins = popular.find_all('article')
+    soup = BeautifulSoup(html, 'lxml')
+    popular = soup.find_all('section')[1]
+    plugins = popular.find_all('article')
+    for plugin in plugins:
+        name = plugin.find('h2').text
+        url = plugin.find('h2').find('a').get('href')
+        r = plugin.find('span', class_='rating-count').find('a').text
+        rating = refined(r)
 
-	for plugin in plugins:
-		name = plugin.find('h2').text
-		url = plugin.find('h2').find('a').get('href')
-		r = plugin.find('span', class_='rating-count').find('a').text
-		rating = refined(r)
-
-		data = {'name': name,
-				'url': url,
-				'reviews': rating }
-		write_csv(data)
-
+        data = {'name': name,
+                'url': url,
+                'reviews': rating }
+        write_csv(data)
 
 
 def main():
-	url = 'https://www.wordpress.org/plugins/'
-	get_data(get_html(url))
-
-
-
+    url = 'https://www.wordpress.org/plugins/'
+    get_data(get_html(url))
 
 
 if __name__ == '__main__':
-	main()
+    main()
